9e812e5b4aff7663b5b92a467a8881415ac45299
Restyled.io <commits@restyled.io>
2022-12-10 23:26:15 +0000

    Restyled by clang-format

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,147 +2,138 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('app', () => {
-    describe("Initializations", () => {
-        it('should initialize the server', () => {
-            expect(app).toBeDefined();
-        });
+  describe("Initializations", () => {
+    it('should initialize the server', () => { expect(app).toBeDefined(); });
+  });
+
+  describe("GET /api/products", () => {
+    it('should return a 200 status code', async () => {
+      const response = await request(app).get('/api/products');
+      expect(response.statusCode).toBe(200);
     });
+  });
 
-    describe("GET /api/products", () => {
-        it('should return a 200 status code', async () => {
-            const response = await request(app).get('/api/products');
-            expect(response.statusCode).toBe(200);
-        });
+  describe("GET /api/products/:id", () => {
+    it('should return a 200 status code', async () => {
+      const response = await request(app).get('/api/products/1');
+      expect(response.statusCode).toBe(200);
     });
+  });
 
-    describe("GET /api/products/:id", () => {
-        it('should return a 200 status code', async () => {
-            const response = await request(app).get('/api/products/1');
-            expect(response.statusCode).toBe(200);
-        });
-    }
-    );
-
-    describe("POST /api/products", () => {
-        it('should return a 200 status code', async () => {
-            const product = {
-                product_name: "Basketball",
-                price: 200.00,
-                stock: 3,
-                tagIds: [1, 2, 3, 4]
-            };
-            const response = await request(app).post('/api/products').send(product);
-            expect(response.statusCode).toBe(200);
-        });
+  describe("POST /api/products", () => {
+    it('should return a 200 status code', async () => {
+      const product = {
+        product_name : "Basketball",
+        price : 200.00,
+        stock : 3,
+        tagIds : [ 1, 2, 3, 4 ]
+      };
+      const response = await request(app).post('/api/products').send(product);
+      expect(response.statusCode).toBe(200);
     });
+  });
 
-    describe("PUT /api/products/:id", () => {
-        it('should return a 200 status code', async () => {
-            const product = {
-                product_name: "Soceer Ball",
-                price: 500.00,
-                stock: 10,
-                tagIds: [1, 2, 3, 4]
-            };
-            const response = await request(app).put('/api/products/1').send(product);
-            expect(response.statusCode).toBe(200);
-        });
+  describe("PUT /api/products/:id", () => {
+    it('should return a 200 status code', async () => {
+      const product = {
+        product_name : "Soceer Ball",
+        price : 500.00,
+        stock : 10,
+        tagIds : [ 1, 2, 3, 4 ]
+      };
+      const response = await request(app).put('/api/products/1').send(product);
+      expect(response.statusCode).toBe(200);
     });
+  });
 
-    describe("GET /api/tags", () => {
-        it('should return a 200 status code', async () => {
-            const response = await request(app).get('/api/tags');
-            expect(response.statusCode).toBe(200);
-        });
+  describe("GET /api/tags", () => {
+    it('should return a 200 status code', async () => {
+      const response = await request(app).get('/api/tags');
+      expect(response.statusCode).toBe(200);
     });
+  });
 
-    describe("GET /api/tags/:id", () => {
-        it('should return a 200 status code', async () => {
-            const response = await request(app).get('/api/tags/1');
-            expect(response.statusCode).toBe(200);
-        });
-    }
-    );
-
-    describe("POST /api/tags", () => {
-        it('should return a 200 status code', async () => {
-            const tag = {
-                tag_name: "Test Tag",
-            };
-            const response = await request(app).post('/api/tags').send(tag);
-            expect(response.statusCode).toBe(200);
-        });
+  describe("GET /api/tags/:id", () => {
+    it('should return a 200 status code', async () => {
+      const response = await request(app).get('/api/tags/1');
+      expect(response.statusCode).toBe(200);
     });
+  });
 
-    describe("PUT /api/tags/:id", () => {
-        it('should return a 200 status code', async () => {
-            const tag = {
-                tag_name: "New Tag",
-            };
-            const response = await request(app).put('/api/tags/1').send(tag);
-            expect(response.statusCode).toBe(200);
-        });
+  describe("POST /api/tags", () => {
+    it('should return a 200 status code', async () => {
+      const tag = {
+        tag_name : "Test Tag",
+      };
+      const response = await request(app).post('/api/tags').send(tag);
+      expect(response.statusCode).toBe(200);
     });
+  });
 
-    describe("GET /api/categories", () => {
-        it('should return a 200 status code', async () => {
-            const response = await request(app).get('/api/categories');
-            expect(response.statusCode).toBe(200);
-        });
+  describe("PUT /api/tags/:id", () => {
+    it('should return a 200 status code', async () => {
+      const tag = {
+        tag_name : "New Tag",
+      };
+      const response = await request(app).put('/api/tags/1').send(tag);
+      expect(response.statusCode).toBe(200);
     });
+  });
 
-    describe("GET /api/categories/:id", () => {
-        it('should return a 200 status code', async () => {
-            const response = await request(app).get('/api/categories/1');
-            expect(response.statusCode).toBe(200);
-        });
-    }
-    );
-
-    describe("POST /api/categories", () => {
-        it('should return a 200 status code', async () => {
-            const category = {
-                category_name: "Test Category",
-            };
-            const response = await request(app).post('/api/categories').send(category);
-            expect(response.statusCode).toBe(200);
-        });
+  describe("GET /api/categories", () => {
+    it('should return a 200 status code', async () => {
+      const response = await request(app).get('/api/categories');
+      expect(response.statusCode).toBe(200);
     });
+  });
 
-    describe("PUT /api/categories/:id", () => {
-        it('should return a 200 status code', async () => {
-            const category = {
-                category_name: "New Category T",
-            };
-            const response = await request(app).put('/api/categories/1').send(category);
-            expect(response.statusCode).toBe(200);
-        });
+  describe("GET /api/categories/:id", () => {
+    it('should return a 200 status code', async () => {
+      const response = await request(app).get('/api/categories/1');
+      expect(response.statusCode).toBe(200);
     });
+  });
 
-
-    describe("DELETE /api/products/:id", () => {
-        it('should return a 200 status code', async () => {
-            const response = await request(app).delete('/api/products/1');
-            expect(response.statusCode).toBe(200);
-        });
+  describe("POST /api/categories", () => {
+    it('should return a 200 status code', async () => {
+      const category = {
+        category_name : "Test Category",
+      };
+      const response =
+          await request(app).post('/api/categories').send(category);
+      expect(response.statusCode).toBe(200);
     });
+  });
 
-    describe("DELETE /api/tags/:id", () => {
-        it('should return a 200 status code', async () => {
-            const response = await request(app).delete('/api/tags/1');
-            expect(response.statusCode).toBe(200);
-        });
+  describe("PUT /api/categories/:id", () => {
+    it('should return a 200 status code', async () => {
+      const category = {
+        category_name : "New Category T",
+      };
+      const response =
+          await request(app).put('/api/categories/1').send(category);
+      expect(response.statusCode).toBe(200);
     });
+  });
 
-    
-    describe("DELETE /api/categories/:id", () => {
-        it('should return a 200 status code', async () => {
-            const response = await request(app).delete('/api/categories/1');
-            expect(response.statusCode).toBe(200);
-        });
+  describe("DELETE /api/products/:id", () => {
+    it('should return a 200 status code', async () => {
+      const response = await request(app).delete('/api/products/1');
+      expect(response.statusCode).toBe(200);
     });
+  });
 
+  describe("DELETE /api/tags/:id", () => {
+    it('should return a 200 status code', async () => {
+      const response = await request(app).delete('/api/tags/1');
+      expect(response.statusCode).toBe(200);
+    });
+  });
 
-
-
+  describe("DELETE /api/categories/:id", () => {
+    it('should return a 200 status code', async () => {
+      const response = await request(app).delete('/api/categories/1');
+      expect(response.statusCode).toBe(200);
+    });
+  });
 });
--- server.js
+++ server.js
@@ -3,6 +3,4 @@ const app = require("./app");
 const PORT = process.env.PORT || 3001;
 
 // Listener
-app.listen(PORT, () => {
-    console.log(`App listening on PORT ${PORT}`);
-});
+app.listen(PORT, () => { console.log(`App listening on PORT ${PORT}`); });
