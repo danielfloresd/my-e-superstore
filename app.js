d70f84af6399d0abcad883bc2657de137642d7e2
Daniel Flores D <danielfloresd@GEO-W3KC67eDAdA.lgs-net.com>
2022-12-10 17:34:02 -0700

    Add simple index.html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
 // import sequelize connection
 
 const app = express();
@@ -8,6 +9,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.use(routes);
+
+
 
 module.exports = app;
